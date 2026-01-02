82bfd5e7a8d5d54c54f0d509d3bbe606198bf23f
NguyenVanNam121204 <nt0143436946@gmail.com>
2026-01-02 17:01:53 +0700

    appjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
 import { EnumProvider } from "./Context/EnumContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
